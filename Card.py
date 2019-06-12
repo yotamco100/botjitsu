57abe1567f5c65b230d2332c35b29c11c079552e
Yotam Cohen <isyotamc@gmail.com>
2019-06-12 18:53:31 +0300

    Added card battle function

--- Card.py
+++ Card.py
@@ -17,7 +17,7 @@ class Element(object):
     WATER = 1
     SNOW = 2
 
-    beats = {FIRE: SNOW, SNOW: WATER, WATER: FIRE}
+    beats_arr = {FIRE: SNOW, SNOW: WATER, WATER: FIRE}
 
     char2elem = {'F': FIRE, 'W': WATER, 'S': SNOW}
     elem2char = {FIRE: 'F', WATER: 'W', SNOW: 'S'}
@@ -25,9 +25,9 @@ class Element(object):
 
     @staticmethod
     def beats(elem1, elem2):
-        if beats[elem1] == elem2:
+        if Element.beats_arr[elem1] == elem2:
             return 1
-        elif beats[elem2] == elem1:
+        elif Element.beats_arr[elem2] == elem1:
             return 2
         else:
             return 0
@@ -50,9 +50,17 @@ class Card(object):
         return "Card Element: {} \tCard Color: {} \tCard Level: {}".format(Element.elem2str[self.element], Color.color2str[self.color], self.number)
 
     @staticmethod
-    def battle(card1, card2):
-        if card1.type == card2.type:
-            pass
+    def battle(card1, card2, is_reversed):
+        elem_out = Element.beats(card1.element, card2.element)
+        if elem_out == 0:
+            if card1.number == card2.number:
+                return 0
+            if (card1.number > card2.number) ^ is_reversed:
+                return 1
+            return 2
+        else:
+            return elem_out
+            
 
 class Deck(object):
 
@@ -77,7 +85,7 @@ class Deck(object):
 if __name__ == "__main__":
     newDeck = Deck()
     print(len(newDeck.deck))
-    hand = newDeck.deal()
-    for card in hand:
-        print(card)
-    print(len(newDeck.deck))
+    card1 = Card(Element.SNOW, 'g', 5)
+    card2 = Card(Element.SNOW, 'b', 5)
+    print("Card1:{}\ncard2:{}".format(card1, card2))
+    print(Card.battle(card1, card2, False))
