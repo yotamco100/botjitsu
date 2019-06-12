db843b3b0c0266e5e80ee90fc8a7fdc4ef2dca5e
Yotam Cohen <isyotamc@gmail.com>
2019-06-12 21:22:56 +0300

    Added documentation for Player.py

--- Player.py
+++ Player.py
@@ -1,8 +1,19 @@
+# Authors: @CiniMinies and @yotamco100
+# A Player object for Card-Jitsu.
+
 from Card import *
 
 class Player(object):
-
+    """
+    A Player class. Represents a single player in the game.
+    """
     def __init__(self):
+        """
+        Creates a Player instance. Inits a Deck and deals 4 cards.
+
+        Gets None.
+        Returns a Player instance.
+        """
         self.deck = Deck()
         self._hand = self.deck.deal()
         self.won_cards = []
@@ -10,6 +21,10 @@ class Player(object):
 
     @property
     def pretty_hand(self):
+        """
+        Pretty Hand Property.
+        Returns a pretty-printed version of the Player's hand.
+        """
         hand_str = ""
         for index, card in enumerate(self._hand):
             hand_str += str(index) + "." + " " + str(card) + "\n"
@@ -17,22 +32,45 @@ class Player(object):
 
     @property
     def hand(self):
+        """
+        Hand Property.
+        Returns the Player's hand in csv format, where each
+        Card is represented in Config syntax.
+        """
         hand_str = ""
         for index, card in enumerate(self._hand):
             hand_str += card.config + ","
         return hand_str[:-1]
         
     def start_turn(self):
+        """
+        Starts the turn by drawing a card.
+
+        Gets None.
+        Returns none.
+        """
         self._hand.append(self.deck.draw())
         #print(self.pretty_hand)
     
     def choose_card(self, card_index):
+        """
+        Plays a card from the Player's hand.
+
+        Gets the Card's index in the hand.
+        Returns the chosen Card instance.
+        """
         chosen = self._hand[card_index]
         self._hand.remove(chosen)
         #print(self.pretty_hand)
         return chosen
 
     def check_win(self, card):
+        """
+        Checks if the Player won the game, using @CiniMinis set theory skills.
+
+        Gets the played Card.
+        Returns if the player won the game in boolean form.
+        """
         self.won_cards.append(card)
         self.element_sets[card.element].add(card.color)
         if (len(self.element_sets[card.element]) == 3):
@@ -47,12 +85,3 @@ class Player(object):
         if len(sub21) > 0 and len((other1 - sub21) - set(card.color)) > 0:
             return True
         return False
-        
-    
-"""
-my_player = Player()
-my_player.start_turn()
-print(my_player.hand + "\n")
-chosen_card = my_player.choose_card(3)
-print("\n" + chosen_card.config)
-"""
