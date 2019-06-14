e11eba91ec9b789dcb65bb61a482a96bf13f0d59
Yotam Cohen <isyotamc@gmail.com>
2019-06-14 20:07:13 +0300

    started work on the python lib

---
+++ python-lib/Card.py
@@ -0,0 +1,143 @@
+# Authors: @CiniMinis and @yotamco100
+# Card-Jitsu classes
+
+import random
+
+
+class Color(object):
+    """
+    A Card Color Enum, including conversions to strings.
+    """
+    RED = 'r'
+    BLUE = 'b'
+    GREEN = 'g'
+    YELLOW = 'y'
+    ORANGE = 'o'
+    PURPLE = 'p'
+
+    color2str = {RED:"Red", BLUE:"Blue", GREEN:"Green", YELLOW:"Yellow", ORANGE:"Orange", PURPLE:"Purple"}
+
+
+class Element(object):
+    """
+    A Card Element Enum, including conversions to chars and strings.
+    """
+    FIRE = 0
+    WATER = 1
+    SNOW = 2
+
+    beats_arr = {FIRE: SNOW, SNOW: WATER, WATER: FIRE}
+
+    char2elem = {'F': FIRE, 'W': WATER, 'S': SNOW}
+    elem2char = {FIRE: 'F', WATER: 'W', SNOW: 'S'}
+    elem2str = {FIRE: 'Fire', WATER: 'Water', SNOW: 'Snow'}
+
+    @staticmethod
+    def beats(elem1, elem2):
+        """
+        An element battle function.
+        
+        Gets two Elements.
+        Returns the winning Player's number, or 0 if stalemate.
+        """
+        if Element.beats_arr[elem1] == elem2:
+            return 1
+        elif Element.beats_arr[elem2] == elem1:
+            return 2
+        else:
+            return 0
+    
+
+
+
+class Card(object):
+    """
+    A Card object. Represents a single Card-Jitsu card.
+    """
+    def __init__(self, element, color, number):
+        """
+        Creates a new Card.
+
+        Gets an Element (from Enum), Color (from Enum), and number(int).
+        Returns a new Card instance.
+        """
+        self.element = element
+        self.color = color
+        self.number = number
+
+    @property
+    def config(self):
+        """
+        Config property.
+        Returns the card's config in syntax: [element char][color char][number char, in hex].
+        """
+        return Element.elem2char[self.element] + self.color + hex(self.number)[2].upper()
+
+    def __str__(self):
+        """
+        Card toString.
+        Returns a pretty-printed string of the Card object.
+        """
+        return "Card Element: {} \tCard Color: {} \tCard Level: {}".format(Element.elem2str[self.element], Color.color2str[self.color], self.number)
+
+    @staticmethod
+    def battle(card1, card2, is_reversed):
+        """
+        Determines the winner of two Card objects.
+
+        Gets two Cards, is_reversed boolean (game state where lower number wins).
+        Returns the winning player's number.
+        """
+        elem_out = Element.beats(card1.element, card2.element)
+        if elem_out == 0:
+            if card1.number == card2.number:
+                return 0
+            if (card1.number > card2.number) ^ is_reversed:
+                return 1
+            return 2
+        else:
+            return elem_out
+            
+
+class Deck(object):
+    """
+    A Deck object. Represents a Deck of Cards, where Cards can be drawn from.
+    """
+    def __init__(self):
+        """
+        Creates a new deck.
+        
+        Opens a deck config file and reads Cards in
+        Config syntax, explained above, then shuffles
+        the Deck.
+
+        Gets None.
+        Returns a Deck instance.
+        """
+        self.deck = []
+        with open("deck.dcfg", "r") as config:
+            for line in config:
+                elem = Element.char2elem[line[0]]
+                color = line[1]
+                number = int(line[2], base=16)
+                self.deck.append(Card(elem, color, number))
+                # print(self.deck[-1])
+        random.shuffle(self.deck)
+
+    def draw(self):
+        """
+        Draw one Card from the Deck.
+
+        Gets None.
+        Returns the top Card and removes it from the Deck.
+        """
+        return self.deck.pop()
+    
+    def deal(self, card_num=4):
+        """
+        Draw card_num Card from the Deck. Used at start of game.
+
+        Gets the number of cards to be drawn.
+        Returns the top card_num Cards and removes them from the Deck.
+        """
+        return [self.draw() for _ in range(card_num)]
---
+++ python-lib/Player.py
@@ -0,0 +1,90 @@
+# Authors: @CiniMinis and @yotamco100
+# A Player object for Card-Jitsu.
+
+from Card import *
+
+class Player(object):
+    """
+    A Player class. Represents a single player in the game.
+    """
+    def __init__(self):
+        """
+        Creates a Player instance. Inits a Deck and deals 4 cards.
+
+        Gets None.
+        Returns a Player instance.
+        """
+        self.deck = Deck()
+        self._hand = self.deck.deal()
+        self.won_cards = []
+        self.element_sets = {Element.FIRE: set(), Element.WATER: set(), Element.SNOW: set()}
+
+    @property
+    def pretty_hand(self):
+        """
+        Pretty Hand Property.
+        Returns a pretty-printed version of the Player's hand.
+        """
+        hand_str = ""
+        for index, card in enumerate(self._hand):
+            hand_str += str(index) + "." + " " + str(card) + "\n"
+        return hand_str
+
+    @property
+    def hand(self):
+        """
+        Hand Property.
+        Returns the Player's hand in csv format, where each
+        Card is represented in Config syntax.
+        """
+        hand_str = ""
+        for index, card in enumerate(self._hand):
+            hand_str += card.config + ","
+        return hand_str[:-1]
+        
+    def start_turn(self):
+        """
+        Starts the turn by drawing a card.
+
+        Gets None.
+        Returns none.
+        """
+        self._hand.append(self.deck.draw())
+        #print(self.pretty_hand)
+    
+    def choose_card(self, card_index):
+        """
+        Plays a card from the Player's hand.
+
+        Gets the Card's index in the hand.
+        Returns the chosen Card instance.
+        """
+        try:
+            chosen = self._hand[card_index]
+            self._hand.remove(chosen)
+        except KeyError:
+            chosen = self._hand[random.randint(0,4)]
+        #print(self.pretty_hand)
+        return chosen
+
+    def check_win(self, card):
+        """
+        Checks if the Player won the game, using @CiniMinis set theory skills.
+
+        Gets the played Card.
+        Returns if the player won the game in boolean form.
+        """
+        self.won_cards.append(card)
+        self.element_sets[card.element].add(card.color)
+        if (len(self.element_sets[card.element]) == 3):
+            return True
+        other1 = self.element_sets[(card.element + 1) % 3]
+        other2 = self.element_sets[(card.element + 2) % 3]
+        
+        sub12 = (other1 - other2) - set(card.color)
+        if len(sub12) > 0 and len((other2 - sub12) - set(card.color)) > 0:
+            return True
+        sub21 = (other2 - other1) - set(card.color)
+        if len(sub21) > 0 and len((other1 - sub21) - set(card.color)) > 0:
+            return True
+        return False
