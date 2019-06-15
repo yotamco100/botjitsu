2dfd18fe6e1c4cdf0d9e1736a920bed63001ee38
Yotam Cohen <isyotamc@gmail.com>
2019-06-15 18:06:55 +0300

    Migrated FULL_DECK and removed OP remaining function

--- python-lib/botjitsu.py
+++ python-lib/botjitsu.py
@@ -2,6 +2,8 @@
 # A Python library for the BotJitsu tournament
 
 from Player import *
+from json import load
+from cards import *
 #import bot_code
 
 
@@ -12,7 +14,16 @@ class Game(object):
     Includes constants, functions, and game
     state variables for ease of use as an API.
     """
-    FULL_DECK = cards.load(open('deck.json'))['decks']
+    FULL_DECK = []
+
+    with open('deck.json') as deck_file:
+        decks = load(deck_file)
+
+        for card in decks['cards']:
+            element = Elements(card['element'])
+            color = Colors(card['color'])
+            number = int(card['number'])
+            FULL_DECK.append(Card(element, color, number))
 
     def __init__(self, my_number, my_hand_config):
         #TODO: implement game data
@@ -21,24 +32,28 @@ class Game(object):
         self.enemy_player = Player([])  # since you don't know the enemy's hand.
         # enemy_player can keep track of the opponent's won_cards.
 
+    """
+    # NOTE: Too overpowered?
     @staticmethod
     def cards_remaining_in_deck(won_cards):
-        """
+        "#""
         Given a won_cards string, determines what cards remain in the deck.
 
-        Gets: won_cards(str[], config strings)
-        Returns: remaining_in_deck(str[], config strings)
-        """
+        Gets: won_cards(Card[])
+        Returns: remaining_in_deck(Card[])
+        "#""
         remaining_deck = list(Game.FULL_DECK)
         for card in won_cards:
             remaining_deck.remove(card)
         return remaining_deck
 
+    """
+
     def _update_hand(self, new_hand):
         """
         updates the Player's hand in the game object.
 
-        Gets: new_hand(str[], config syntax)
+        Gets: new_hand(str[], new-age config syntax)
         Returns: None.
         """
         self.my_player._hand = [cards.Card.cfg2card(new_card) for new_card in new_hand]
@@ -47,7 +62,7 @@ class Game(object):
         """
         Updates the Player's won_cards with the winning card.
 
-        Gets: winning_player (int), winning_card(str, config format)
+        Gets: winning_player (int), winning_card(str, new-age config format)
         """
         if winning_player == self.my_number:
             self.my_player.add_to_won_cards(cards.Card.cfg2card(winning_card))
